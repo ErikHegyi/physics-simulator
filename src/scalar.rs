91690e9839c441c7fba7b6048f23b3c7fc120429
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-04-15 15:34:36 +0200

    Moved Scalars to Rust

---
+++ src/scalar.rs
@@ -0,0 +1,112 @@
+use std::fmt::{Display, Formatter};
+use std::ops::{
+    Add,
+    AddAssign,
+    Sub,
+    SubAssign,
+    Mul,
+    MulAssign,
+    Div,
+    DivAssign
+};
+use crate::*;
+
+
+/// # Scalar
+/// A value with only magnitude and no direction
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+#[pyclass]
+pub struct Scalar {
+    pub value: f64
+}
+
+#[pymethods]
+impl Scalar {
+    #[new]
+    pub fn new(value: f64) -> Self {
+        Self { value }
+    }
+
+    pub fn __add__(&self, rhs: Self) -> Self { self.clone() + rhs }
+    pub fn __sub__(&self, rhs: Self) -> Self { self.clone() - rhs }
+    pub fn __mul__(&self, rhs: Self) -> Self { self.clone() * rhs }
+    pub fn __truediv__(&self, rhs: Self) -> Self { self.clone() / rhs }
+}
+
+
+impl Add for Scalar {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value + rhs.value
+        }
+    }
+}
+
+
+impl AddAssign for Scalar {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs
+    }
+}
+
+
+impl Sub for Scalar {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value - rhs.value
+        }
+    }
+}
+
+
+impl SubAssign for Scalar {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs
+    }
+}
+
+
+impl Mul for Scalar {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value * rhs.value
+        }
+    }
+}
+
+
+impl MulAssign for Scalar {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs
+    }
+}
+
+
+impl Div for Scalar {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value / rhs.value
+        }
+    }
+}
+
+
+impl DivAssign for Scalar {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs
+    }
+}
+
+impl Display for Scalar {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.value >= 1e4 {
+            write!(f, "{:e}", self.value)
+        } else {
+            write!(f, "{}", self.value)
+        }
+    }
+}
