ef516e21d97901a4c5c0deaab5940a9c060720f6
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-05-04 13:17:32 +0200

    Moved Scalars to Rust

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,11 @@ use crate::{
 use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
-#[pymodule]
+#[pymodule(name = "physics")]
 fn physics(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Point>()?;
     m.add_class::<Vector>()?;
+    m.add_class::<Scalar>()?;
     m.add_class::<Degree>()?;
     Ok(())
 }
