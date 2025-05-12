16606ef463c5053999cad90d32b17b70410b3c64
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-05-12 18:05:11 +0200

    Moved celestials to Rust, split up planets and stars, stars now emit color based on their temperature (which is based on their mass)

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,21 @@ mod vector;
 mod degree;
 mod scalar;
 mod radiation;
+mod point_body;
+mod constants;
+mod star;
+mod planet;
 
 use crate::{
     point::*,
     scalar::*,
     vector::*,
     degree::*,
-    radiation::*
+    radiation::*,
+    point_body::*,
+    star::*,
+    planet::*,
+    constants::*,
 };
 
 use pyo3::prelude::*;
@@ -22,5 +30,11 @@ fn physics(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Scalar>()?;
     m.add_class::<Degree>()?;
     m.add_class::<Radiation>()?;
+    m.add_class::<Constants>()?;
+    m.add_class::<PointBody>()?;
+    m.add_class::<Star>()?;
+    m.add_class::<StarType>()?;
+    m.add_class::<Planet>()?;
+    m.add_class::<PlanetType>()?;
     Ok(())
 }
