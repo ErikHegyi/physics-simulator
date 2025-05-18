2c8023080dcd462722cef964152a81821616eaaa
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-05-18 18:11:16 +0200

    Updated astronomical_simulation! macro

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ use crate::{
 
 fn main() {
     let mut sim = astronomical_simulation!(
-        star!("Sun", vector!(0), (0, 0, 0), 2e30, 696_340_000),
-        planet!("Mercury", vector!(0, 47_400, 0), point!(58e9, 0, 0), 3.3e22, 2_440_000),
-        planet!("Venus", (0, 35e3, 0), (0, 0, -108e9), 4.87e24, 6052e3),
-        planet!("Earth", (-29780, 0, 0), (0, 0, AU), 5.97e24, 6378e3),
-        planet!("Mars", (0, 0, -24100), (228e9, 0, 0), 6.42e23, 3390e3),
+        vec![
+            Box::new(star!("Sun", vector!(0), (0, 0, 0), 2e30, 696.34e6)),
+            Box::new(planet!("Mercury", (0, 47.4e3, 0), (58e9, 0, 0), 3.3e22, 2.44e6)),
+            Box::new(planet!("Venus", (0, 35e3, 0), (0, 0, -108e9), 4.87e24, 6052e3)),
+            Box::new(planet!("Earth", (-29.78e3, 0, 0), (0, 0, AU), 5.97e24, 6378e3)),
+            Box::new(planet!("Mars", (0, 0, -24.1e3), (228e9, 0, 0), 6.42e23, 3390e3))
+        ]
     );
     sim.run();
 }
